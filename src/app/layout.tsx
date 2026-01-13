efd7e021d19c37c2bd7adf60eaaa32c5c7070914
yushri <yushri.ml@gmail.com>
2026-01-13 18:01:47 +0530

    chore: setup TanStack Query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/providers/query-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <QueryProvider>
+    {children}
+  </QueryProvider>
         {children}
       </body>
     </html>
